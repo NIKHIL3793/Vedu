a34aea85cb574a86c9f07bf47c784e260ff259e3
dev <dev@gmail.com>
2023-02-15 22:24:00 +0530

    public mrethod

---
+++ src/Methodbynikhil.java
@@ -0,0 +1,13 @@
+public class Methodbynikhil
+{
+    public void publicMethod()
+    {
+        String name=("method by nikhil");
+        System.out.println(name);
+    }
+    public static void main(String[] args)
+    {
+        Methodbynikhil obj=new Methodbynikhil();
+        obj.publicMethod();
+    }
+}
