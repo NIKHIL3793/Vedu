4b3a677ef39c249e76ecf498afed7575d5916572
dev <dev@gmail.com>
2023-02-15 22:00:23 +0530

    fall

---
+++ src/Fall.java
@@ -0,0 +1,12 @@
+public class Fall
+{
+    public void publicMethod()
+    {
+        System.out.println("name");
+    }
+    public static void main(String[] args)
+    {
+        Fall obj=new Fall();
+        obj.publicMethod();
+    }
+}
