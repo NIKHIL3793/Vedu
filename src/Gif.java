7e3e126e6abc8b47ab74341004bee2f8fb6ed109
dev <dev@gmail.com>
2023-02-15 22:27:12 +0530

    public mrethod

---
+++ src/Gif.java
@@ -0,0 +1,13 @@
+public class Gif
+{
+    public void publicMethod()
+    {
+        String name=("method by nikhil");
+        System.out.println(name);
+    }
+    public static void main(String[] args)
+    {
+        Gif obj=new Gif();
+        obj.publicMethod();
+    }
+}
