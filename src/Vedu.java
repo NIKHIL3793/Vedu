ba10b0a15e3c7f8fb7add43c24bfb1f697415fbb
dev <dev@gmail.com>
2023-02-15 20:16:09 +0530

    hey nikhil

---
+++ src/Vedu.java
@@ -0,0 +1,10 @@
+public class Vedu
+{
+    public static void main(String[] args) {
+        int a=500;
+        int b =400;
+        int c=5439;
+        int f=a+b+c;
+        System.out.println(f);
+    }
+}
