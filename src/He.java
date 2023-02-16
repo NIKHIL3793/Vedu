2c050d3c7e51a8ee6a0eda029d116b3341459ea0
dev <dev@gmail.com>
2023-02-16 09:05:06 +0530

    public mrethod

---
+++ src/He.java
@@ -0,0 +1,13 @@
+public class He
+{
+    public void publicMethod()
+    {
+        String name="rocky";
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        He obj=new He();
+        obj.publicMethod();
+    }
+}
